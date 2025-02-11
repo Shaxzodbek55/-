4e41820ddf158fc3d7c2ab7ea346ec4852821e6b
Shaxzodbek55 <torakulovshaxzod67@gmail.com>
2025-02-11 16:13:21 +0500

    Add files via upload

---
+++ hatolar ustida ishlash/script.js
@@ -0,0 +1,24 @@
+let name = prompt('ismingizni yozing;')
+let surname= prompt('familyangizni yozing;')
+let age = prompt('yoshingizmi yozing;')
+let idCard = prompt('Id raqamingizni yozing')
+
+
+let countrychoice = prompt('qayerga boriishingizni tanlang(sonni tanlang):\n1 - trkiya\n2 - rossiya \n3 - dubay\n4 - italya \n5');
+
+let countruyimages = {
+    1:'turkiy'
+    2:'russian'
+    3:'dubay'
+    4:'italay'
+
+};
+
+document.getElementById('output').innerHTML
+    <strong>imya:
+
+
+    
+
+
+
